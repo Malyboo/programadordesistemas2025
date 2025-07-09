20863e57d166ce94510d445e04847533adeda61a
Malyboo <nandoluh1990@gmail.com>
2025-07-09 13:56:40 -0300

    Edição no sorteador

--- jogo da velha/python/sorte.py
+++ jogo da velha/python/sorte.py
@@ -6,22 +6,17 @@ import webbrowser
 import tkinter as tk
 from tkinter import messagebox
 
+#FUNÇÃO SORTEAR
 def sortear():
-    opcao = 5
+    opcao = 6
     numSorteado = random.randint(1, opcao)
-    #print("O número sorteado é: ", numSorteado)
-
-    # try:
-    #     escolha = int(input(f"Escolha um número entre 1 e {opcao}: "))
-    #     if escolha < 1 or escolha > opcao:
-    #         raise ValueError("Número fora de intervalo! ")
-    # except ValueError as e:
-    #     print(f"entrada inválida: {e} tente de novo!")
-    #     sortear()
+ 
 
     def verificarEscolha(escolha):
         if escolha == numSorteado:
             print("Bye Bye word, seu pc será desligado!👻 ")
+            messagebox.showerror("Perdeu!", "O computador Será desligado! 👻")
+
             time.sleep(5)
             if sys.platform == "win32":
                 os.system("shutdown /s /t 1")
@@ -32,18 +27,41 @@ def sortear():
 
         else:
             print("Você está seguro, por enquanto! ")
+            messagebox.showinfo("Ufa!","Você está seguro, por enquanto! 😊")
+            sortear()
+            
+
     janela = tk.Toplevel()
-    janela.title("Algoritimo de sorteio")
-    tk.Label(janela, text="Escola um número entre 1 e 6").pack(pady=10)  
+    janela.title("Algoritmo de sorteio")
+    tk.Label(janela, text="Escolha um número entre 1 e 6").pack(pady=10)
 
-    for i in range(1,6):
-        tk.Button(janela, text=str(i), command=lambda i=i: [janela.destroy(), verificarEscolha(i)]).pack(pady=5)      
+    for i in range(1,7):
+        tk.Button(janela, text=str(i), command=lambda i=i: [janela.destroy(),verificarEscolha(i)]).pack(pady=5)
 
+#EXIBIR REGRAS
+def exibirRegras():
+    regras = (
+        "Regras do Jogo: \n"
+        "1. Escolha um número entre 1 e 6. \n"
+        "2. Se você escolher o nº sorteado, o PC será desligado! \n"
+        "3. Se não for, o jogo continua. \n"
+        "4. Boa sorte, vai precisar!"
+    )
+    messagebox.showinfo("Regras", regras)
+
+
+#FUNÇÃO SAIR
+def sair():
+    root.destroy()
 
 root = tk.Tk()
 root.title("Jogo do evento aleatório")
-tk.Label(root, text="Bem-Vindo ao Jogo de Evento Aleatório!", font=("arial", 20)).pack(pady=15)
+tk.Label(root, text="Bem-vindo ao Jogo de Evento Aleatório!", font=("Arial", 20)).pack(pady=15)
 tk.Button(root, text="Iniciar Jogo", width=20, command=sortear).pack(pady=10)
-tk.Button(root, text="Ver regras", width=20, command=sortear).pack(pady=10)
-tk.Button(root, text="Sair", width=20, command=sortear).pack(pady=10)
+tk.Button(root, text="Ver regras", width=20, command=exibirRegras).pack(pady=10)
+tk.Button(root, text="Sair", width=20, command=sair).pack(pady=10)
 root.mainloop()
+
+
+
+
