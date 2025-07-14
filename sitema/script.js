5d90d5efc6c65ee3699abb8d40e1c4eb2d3eab68
Malyboo <nandoluh1990@gmail.com>
2025-07-14 17:16:00 -0300

    Implementar javascript e estiloss

---
+++ sitema/script.js
@@ -0,0 +1,21 @@
+document.getElementById("toggleSenha").addEventListener("click", function () {
+    const senha = document.getElementById("senha");
+    const icone = document.getElementById("iconeOlho");
+
+    if (senha.type === "password") {
+        senha.type = "text";
+        icone.classList.remove("fa-eye");
+        icone.classList.add("fa-eye-slash");
+    } else {
+        senha.type = "password";
+        icone.classList.remove("fa-eye-slash");
+        icone.classList.add("fa-eye");
+    }
+});
+
+1
+2
+3
+let table = new DataTable('#tabela', {
+    // options
+});
