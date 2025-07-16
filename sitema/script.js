0dc16f7d7451c83fb9ad6fbb7d4a6759762a39e5
Malyboo <nandoluh1990@gmail.com>
2025-07-16 17:27:59 -0300

    Tela de login

--- sitema/script.js
+++ sitema/script.js
@@ -18,4 +18,21 @@ document.getElementById("toggleSenha").addEventListener("click", function () {
 3
 let table = new DataTable('#tabela', {
     // options
+    language: {
+        url: 'https://cdn.datatables.net/plug-ins/2.3.2/i18n/pt-BR.json',
+    },
 });
+
+
+// function visualizar(){
+    // var senha = $('#senha')
+    // var icone = $('#olho')
+
+    // if (senha.attr('type') === 'password'){
+        // senha.attr('type', 'text');
+        // icone.removeClass('fa-eye').addClass('fa-eye-slash');
+    // }else{
+        // senha.attr('type, 'password');
+            // icone.removeClass('fa-eye-slash').addClass('fa-eye');
+    // }
+// }
